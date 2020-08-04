f3fae1d40763d611678ce7a8bf4f10a191ff8ffa
Aryan <51026686+Violent-Idiot@users.noreply.github.com>
2020-08-04 18:23:23 +0530

    yup

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
 module.exports = app;
